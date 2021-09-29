8bd978c87c67b6e3a66d3da6490520535fd2ef8e
STEFANOS TRIANTAFYLLIDIS <STT0242@my.londonmet.ac.uk>
2021-09-29 12:54:56 +0100

    More plot additions

--- BasicRefresherGame/MapSys.cpp
+++ BasicRefresherGame/MapSys.cpp
@@ -81,6 +81,7 @@ void MapSys::LocController()
 			SpeechText();
 			//TODO:Include fight here with spamton from Deltarune Because that is the game I played recently and I'm bored
 			//TODO:Make him have High def and low HP but higher Attack than slime
+			SetMapLoc(Map::Shore);
 			break;
 		case Map::Port:
 			std::cout << "You arrive at the port and spot a human ready to depart but he stops and waves at you.." << std::endl;
@@ -100,9 +101,27 @@ void MapSys::LocController()
 			SpeechText();
 			break;
 		case Map::Shore: 
-			break;//Secret item?
-		case Map::Town: 
-			break;//SafeArea
+			std::cout << "You are now at the shore after fighting that weird monster, You look beneath you and find an item in the sand" << std::endl;
+			SpeechText();
+			//add here dialogue that shows the item being in inventory
+			//Same Item as in Town Route for Shield and Magician
+			SetMapLoc(Map::DarkPortal);
+			break;
+		case Map::Town:
+			std::cout << "You have arrived at the Local Town with the help of the Boat-Man" << std::endl;
+			SpeechText();
+			std::cout << "Boat-Man: Alright " << m_player.GetName() << " We have arrived, get your things and get going" << std::endl;
+			SpeechText();
+			std::cout << "You leave the boat and begin looking around the shops of the town, without any money its of no use, but you browse anyway" << std::endl;
+			SpeechText();
+			std::cout << "A lady walks past you and give you a necklace and whispers.." << std::endl;
+			SpeechText();
+			std::cout << "???: Come back here at night, There is big trouble in the Woods.." << std::endl;
+			SpeechText();
+			std::cout << "You heard the lady and wore the necklace, it seems to hold a magical power to it.." << std::endl;
+			SpeechText();
+			//Add Item Acquired text and a boost in Magic and Damage 
+			break;
 		case Map::DarkPortal:
 			break;
 		case Map::CastleOfFire:
