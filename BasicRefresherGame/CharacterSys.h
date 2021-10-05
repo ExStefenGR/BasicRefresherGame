7619a488375998b77f84c20eb4a7bec5e5e8f149
Stefanos Triantafyllidis <stt0242@my.londonmet.ac.uk>
2021-10-05 16:10:39 +0100

    Restoring Map files

---
+++ BasicRefresherGame/CharacterSys.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class CharacterSys
+{
+	enum class Job
+	{
+		Sword = 1,
+		Staff,
+		Bow
+	}; //maybe move this in mapsys as well for easier navigation?
+	enum class Skills
+	{
+		Berserker = 1,
+		Fireball,
+		ArrowShower
+	};
+public:
+	CharacterSys();
+	~CharacterSys();
+
+	std::string GetName();
+	void SetName();
+	void SetDamage(int damagePoints);
+	int GetDamage();
+	void SetHealthPoints(int healthPoints);
+	int GetHealthPoints();
+	Skills GetSkills();
+	void SetManaPoints(int manaPoints);
+	int GetManaPoints();
+	void SetCharacterClass();
+	int GetCharacterClass();
+	void PlayerInfo();
+
+	std::string m_name;
+	int m_damagePoints;
+	int m_healthPoints;
+	int m_manaPoints;
+	int m_class;
+	Job m_job;
+	Skills m_skill;
+};
+
