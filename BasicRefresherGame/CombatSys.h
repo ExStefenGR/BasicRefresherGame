91315bdb0d44854b10c4c358615d24f43e857b53
david-levi-student <dal0531@my.londonmet.ac.uk>
2021-10-19 15:53:35 +0100

    Updating date of last change

--- BasicRefresherGame/CombatSys.h
+++ BasicRefresherGame/CombatSys.h
@@ -4,7 +4,7 @@
 //Staff is the second class, they deal damage with spells, it costs more mana than sword but deals heavier damage in bursts
 //The third class which is shield is used for tank, HP is expotentially increased for the cost of not being able to do spells or skills
 //Combat System by David, with help from Isaac
-//Last Changed: 12/10/21
+//Last Changed: 19/10/21
 
 
 #include <iostream>
