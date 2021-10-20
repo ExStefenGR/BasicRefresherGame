cb2a5eded880b326a0a567c72b42cf595274fe6a
david-levi-student <dal0531@my.londonmet.ac.uk>
2021-10-20 10:17:48 +0100

    Updating the credits on combat

--- BasicRefresherGame/CombatSys.h
+++ BasicRefresherGame/CombatSys.h
@@ -3,8 +3,8 @@
 //Sword is for attacks and consumes mana for skills
 //Staff is the second class, they deal damage with spells, it costs more mana than sword but deals heavier damage in bursts
 //The third class which is shield is used for tank, HP is expotentially increased for the cost of not being able to do spells or skills
-//Combat System by David, with help from Isaac
-//Last Changed: 19/10/21
+//Combat System by David and Isaac
+//Last Changed: 20/10/21
 
 
 #include <iostream>
